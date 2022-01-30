e4488447ca29988f9de59219e051414fd432119c
Felipe Castro <fpocastro@gmail.com>
2022-01-30 00:45:34 -0300

    added spinner component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Spinner() {
+  return (
+    <div className="w-8 h-8 border-4 border-gray-400 border-solid rounded-full animate-spin border-t-transparent" />
+  );
+}
