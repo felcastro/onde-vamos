fae901d95ddecb644152e2022173ec14f3c153e5
Felipe Castro <fpocastro@gmail.com>
2022-01-29 22:21:44 -0300

    added navigation component

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navigation() {
+  return (
+    <div className="flex justify-center items-center h-16 w-full bg-gray-700 text-gray-50 shadow-md">
+      <div>Onde Vamos?</div>
+    </div>
+  );
+}
