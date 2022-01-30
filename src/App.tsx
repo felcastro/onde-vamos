01328033407b3a16ee66d63831915da4395e184e
Felipe Castro <fpocastro@gmail.com>
2022-01-29 22:22:45 -0300

    main app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
 
-function App() {
-  return <div>test</div>;
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="h-full flex flex-col">
+        <div className="flex-1 overflow-y-scroll mx-2 mt-2">
+          <Home />
+        </div>
+        <Navigation />
+      </div>
+    </QueryClientProvider>
+  );
 }
-
-export default App;
