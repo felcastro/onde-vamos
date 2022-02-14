216b856076adaae5244f09c5842e83385de0f496
Felipe Castro <fpocastro@gmail.com>
2022-02-14 00:47:34 -0300

    added partial bottom navigation icons

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,23 +1,28 @@
-import React, { useState } from "react";
-import { FaPlus } from "react-icons/fa";
-import CreatePlaceModal from "../CreatePlaceModal";
+import React, { LinkHTMLAttributes } from "react";
+import { FaStoreAlt, FaCog } from "react-icons/fa";
+
+interface NavigationIconButtonProps
+  extends LinkHTMLAttributes<HTMLAnchorElement> {
+  icon: JSX.Element;
+}
+
+function NavigationIconButton({ icon, ...props }: NavigationIconButtonProps) {
+  return (
+    <a className="text-4xl" {...props}>
+      {icon}
+    </a>
+  );
+}
 
 export default function Navigation() {
-  const [isCreatePlaceModalOpen, setIsCreatePlaceModalOpen] = useState(false);
-
   return (
-    <div className="absolute bottom-0 flex justify-center items-center h-16 w-full bg-gray-900 bg-opacity-75 text-gray-50 shadow-md">
-      <div>Onde Vamos?</div>
-      <button
-        className="btn btn-outline btn-rounded absolute right-2"
-        onClick={() => setIsCreatePlaceModalOpen(true)}
-      >
-        <FaPlus />
-      </button>
-      <CreatePlaceModal
-        isOpen={isCreatePlaceModalOpen}
-        onClose={() => setIsCreatePlaceModalOpen(false)}
-      />
+    <div className="absolute bottom-0 flex h-16 w-full">
+      <div className="relative flex flex-1 bg-gray-900 bg-opacity-75 text-gray-50 shadow-md">
+        <div className="flex flex-1 justify-evenly items-center gap-4 px-4 py-2">
+          <NavigationIconButton href="/" icon={<FaStoreAlt />} />
+          <NavigationIconButton href="/" icon={<FaCog />} />
+        </div>
+      </div>
     </div>
   );
 }
